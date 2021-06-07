974f24b745b34c8d118a57d3fa230363d090d2f9
Edvanio Filho <j.edvaniofilho@gmail.com>
2021-06-07 11:23:59 -0300

    contentfull setup

---
+++ setup/credentials.js
@@ -0,0 +1,2 @@
+export const space_id = "gzs22y5r9in3";
+export const access_token = "tEx4dSbDSuRxaGhi7TthTuna1zgge-cLROZgImy0qIE";
---
+++ setup/main.js
@@ -0,0 +1,5 @@
+const space_id = "gzs22y5r9in3";
+const access_token = "tEx4dSbDSuRxaGhi7TthTuna1zgge-cLROZgImy0qIE";
+
+const endpoint = "https://graphql.contentful.com/content/v1/spaces/" + space_id;
+
